55991a1694705a5652b337087f947634e766e16f
台运鹏 <yunpengtai@foxmail.com>
2022-01-13 20:04:32 +0800

    Update helper.py

--- helper.py
+++ helper.py
@@ -9,7 +9,7 @@ def set_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = True
+    torch.backends.cudnn.benchmark = False
     os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.backends.cudnn.deterministic = True
 
